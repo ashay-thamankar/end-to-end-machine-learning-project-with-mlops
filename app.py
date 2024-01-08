619093c5f4974579763cc939df3aa31f0c687f23
ashay-thamankar <ashaythamankar@gmail.com>
2024-01-08 22:33:20 +0530

    prediction added

--- app.py
+++ app.py
@@ -46,5 +46,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
-    # app.run(host='0.0.0.0', port=8080)
+    # app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080)
